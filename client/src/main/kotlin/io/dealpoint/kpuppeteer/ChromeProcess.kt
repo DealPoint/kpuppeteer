ab2d5757876e63d4fd61b3c522b26915b22f1f66
Nick Sherred <nick@Dealpoint.io>
2018-04-23 15:41:59 -0700

    #2: add kpuppeteer entry point and add some project deployment setup

---
+++ client/src/main/kotlin/io/dealpoint/kpuppeteer/ChromeProcess.kt
@@ -0,0 +1,48 @@
+package io.dealpoint.kpuppeteer
+
+import io.dealpoint.kpuppeteer.utils.logger
+import java.io.File
+
+const val DEFAULT_CHROME_INSTALLATION = "/Applications/Google Chrome.app/Contents/MacOS/"
+
+class ChromeProcess(pathToChrome: String? = DEFAULT_CHROME_INSTALLATION) : AutoCloseable {
+  private val port = 9292
+  private val chromeOptions = listOf(
+    "./Google Chrome", "--headless", "--disable-gpu",
+    "--remote-debugging-port=$port", "--crash-dumps-dir=/tmp")
+  private val process = ProcessBuilder(chromeOptions)
+    .directory(File(pathToChrome))
+    .start()
+
+  private val errorStream = process.errorStream
+
+  val webSocketURL = this.getChromeWebSocketURL()
+  val hostPort = webSocketURL.substringBefore("/devtools/browser/")
+
+  override fun close() {
+    this.errorStream.close()
+    this.process.destroyForcibly()
+  }
+
+  private fun getChromeWebSocketURL(): String {
+    log.info("attempting to get chrome web socket url from stderr")
+    val regex = Regex("(ws:\\/\\/.*)")
+    val errorStreamReader = errorStream.bufferedReader()
+    do {
+      val line = errorStreamReader.readLine()
+      log.info(line)
+      val match = regex.find(line)
+      if (match !== null) {
+        errorStreamReader.close()
+        return match.value
+      }
+      log.info(errorStreamReader.ready().toString())
+    } while (errorStreamReader.ready())
+    errorStreamReader.close()
+    throw Error("could not find web socket url in stderr")
+  }
+
+  companion object {
+    private val log = logger()
+  }
+}
--- client/src/main/kotlin/io/dealpoint/kpuppeteer/KPuppeteer.kt
+++ client/src/main/kotlin/io/dealpoint/kpuppeteer/KPuppeteer.kt
@@ -1,23 +1,41 @@
 package io.dealpoint.kpuppeteer
 
+import io.dealpoint.kpuppeteer.generated.PageDomain
 import io.dealpoint.kpuppeteer.utils.logger
-import java.io.File
+import java.util.concurrent.TimeUnit
 
-const val DEFAULT_CHROME_INSTALLATION = "/Applications/Google Chrome.app/Contents/MacOS/"
+class KPuppeteer(val transportManager: TransportManager) : AutoCloseable {
 
-class KPuppeteer(
-  pathToChrome: String? = DEFAULT_CHROME_INSTALLATION) {
+  fun newPage(): PageDomain {
+    return goto("about:blank")
+  }
 
-  private val port = 9292
-  private val chromeOptions = listOf(
-    "./Google Chrome", "--headless",
-    "--remote-debugging-port=$port", "--crash-dumps-dir=/tmp")
-  private val process = ProcessBuilder(chromeOptions)
-    .directory(File(pathToChrome))
-    .start()
-  private val errorStream = process.errorStream
+  fun goto(url: String): PageDomain {
+    log.debug("attempting to load: $url")
+    val targetId = this.createTargetId(url)
+    val transport = transportManager.newTransport("page", targetId)
+    val page = transport.page
+    val target = transport.target
+    target.getTargetInfo(targetId).get(10000, TimeUnit.MILLISECONDS)
+    page.enable().get(10000, TimeUnit.MILLISECONDS)
+    return page
+  }
+
+  private fun createTargetId(url: String): String {
+    val target = transportManager.getCurrentTransport().target
+    val contextId = target.createBrowserContext().get().browserContextId
+    return target.createTarget(url, browserContextId = contextId).get().targetId
+  }
+
+  override fun close() {
+    transportManager.close()
+  }
 
   companion object {
     private val log = logger()
+    fun launch(): KPuppeteer {
+      val rpcManager = ChromeTransportManager(ChromeProcess())
+      return KPuppeteer(rpcManager)
+    }
   }
 }
